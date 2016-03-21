9d2b33fee6a62588fc9c944edf50f8da845db91a
coolworld2015 <wintermute2015@ukr.net>
2016-03-21 14:39:44 +0200

    added NameService

--- app/app.component.js
+++ app/app.component.js
@@ -1,11 +1,49 @@
 (function (app) {
-    app.AppComponent =
-        ng.core.Component({
-            selector: 'my-app',
-            template: '<h1>My First Angular 2 App !!!</h1>'
+
+    var NameService = function () {
+    };
+
+    NameService.prototype.greeting = function () {
+        return 'Edward Tkachuk';
+    };
+
+    var MyName = ng.core
+        .Component({
+            selector: 'my-name',
+            providers: [NameService],
+            template: '<div>{{greeting}}</div>'
+        })
+        .Class({
+            constructor: [NameService, function (service) {
+                this.greeting = service.greeting();
+            }]
+        });
+
+    var Footer = ng.core
+        .Component({
+            selector: 'footer',
+            template: '<br>'
         })
         .Class({
             constructor: function () {
             }
         });
+
+    app.AppComponent =
+        ng.core.Component({
+            selector: 'my-app',
+            template: '<h1>Hello Angular 2!</h1>' +
+            '<footer></footer>' +
+            '<my-name></my-name>',
+            directives: [Footer, MyName]
+        })
+            .Class({
+                constructor: function () {
+                }
+            });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        ng.platform.browser.bootstrap(app.AppComponent);
+    });
+
 })(window.app || (window.app = {}));
--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 (function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platform.browser.bootstrap(app.AppComponent);
-  });
+  //document.addEventListener('DOMContentLoaded', function() {
+  //  ng.platform.browser.bootstrap(app.AppComponent);
+  //});
 })(window.app || (window.app = {}));
